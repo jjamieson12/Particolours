f169d2d2bac609bd73f60bd689eb39b45c6d034b
jjamieson12 <j.jamieson.1@research.gla.ac.uk>
2022-12-15 21:50:00 +0000

    expanded piechart plotting

--- config/HelperFunctions.py
+++ config/HelperFunctions.py
@@ -1,2 +1,39 @@
 def printdict(dict):
     [print(key,':',value) for key, value in dict.items()]
+
+ColourMap = {}
+ColourMap["red"] = "#FF2600"
+ColourMap["green"] = "#4DCA45"
+ColourMap["blue"] = "#003661"
+ColourMap["yellow"] = "#FFFB00"
+ColourMap["orange"] = "#FF9300"
+ColourMap["purple"] = "#942192"
+ColourMap["pink"] = "#FF52D1"
+ColourMap["brown"] = "#AA7942"
+ColourMap["black"] = "#000000"
+ColourMap["white"] = "#FFFFFF"
+ColourMap["grey"] = "#404040"
+ColourMap["gold"] = "#FFCE67"
+ColourMap["silver"] = "#A5AEAB"
+ColourMap["cyan"] = "#00FDFF"
+ColourMap["magenta"] = "#FF40FF"
+ColourMap["beige"] = "#A7987F"
+ColourMap["colourless"] = "#FFFFFF"
+ColourMap["others"] = "#397EBE"
+
+NameMap = {}
+NameMap["Higgs boson"] = "Higgs"
+NameMap["W boson(s)"] = "W boson"
+NameMap["Bottom quark"] = "Bottom"
+NameMap["Charm quark"] = "Charm"
+NameMap["Top quark"] = "Top"
+NameMap["Up quark"] = "Up"
+NameMap["Tau lepton"] = "Tau"
+NameMap["Strange quark"] = "Strange"
+NameMap["Proton"] = "Proton"
+NameMap["Z boson"] = "Z boson"
+NameMap["Muon"] = "Muon"
+NameMap["Electron"] = "Electron"
+NameMap["Neutron"] = "Neutron"
+NameMap["Down quark"] = "Down"
+
